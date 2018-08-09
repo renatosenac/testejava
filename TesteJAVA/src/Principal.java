bb422f3c4a2a5010fb94373954bc5ca1fdad2290
renatosenac <42238475+renatosenac@users.noreply.github.com>
2018-08-09 07:38:53 -0300

    Primeiro commit

---
+++ TesteJAVA/src/Principal.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Testando...");
+	}
+
+}
