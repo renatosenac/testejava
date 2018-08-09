8e5269f002fe63828de1a4f38e70ced3b8a9fe1b
renatosenac <42238475+renatosenac@users.noreply.github.com>
2018-08-09 11:47:58 +0000

    Ajuste texto

--- TesteJAVA/src/Principal.java
+++ TesteJAVA/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "Testandmo...");
+		JOptionPane.showMessageDialog(null, "Testando...");
 	}
 
 }
